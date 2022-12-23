43b3e59dbbbf35d4302a27bfffe49efa488674de
Paulo Sousa <paulosousa@cloud.upwork.com>
2022-12-23 13:37:33 +0100

    Add cnpj aos parametros de configuracao

--- android/src/main/kotlin/com/loopmarket/clisitef/ClisitefPlugin.kt
+++ android/src/main/kotlin/com/loopmarket/clisitef/ClisitefPlugin.kt
@@ -59,7 +59,7 @@ class ClisitefPlugin: FlutterPlugin, MethodCallHandler {
       "setPinpadDisplayMessage" -> pinPadMethods.setDisplayMessage(call.argument<String>("message")!!)
       "pinpadReadYesNo" -> pinPadMethods.readYesOrNo(call.argument<String>("message")!!)
       "pinpadIsPresent" -> pinPadMethods.isPresent()
-      "configure" -> tefMethods.configure(call.argument<String>("enderecoSitef")!!, call.argument<String>("codigoLoja")!!, call.argument<String>("numeroTerminal")!!, "[TipoPinPad=Android_AUTO;]")
+      "configure" -> tefMethods.configure(call.argument<String>("enderecoSitef")!!, call.argument<String>("codigoLoja")!!, call.argument<String>("numeroTerminal")!!, "[TipoPinPad=Android_AUTO;ParmsClient=1="+call.argument<Int>("cnpjLoja")!!+";2="+call.argument<Int>("cnpjEmpresa")!!+"]")
       "getQttPendingTransactions" -> tefMethods.getQttPendingTransactions(call.argument<String>("dataFiscal")!!, call.argument<String>("cupomFiscal")!!)
       "startTransaction" -> tefMethods.startTransaction(cliSiTefListener, call.argument<Int>("modalidade")!!, call.argument<String>("valor")!!, call.argument<String>("cupomFiscal")!!, call.argument<String>("dataFiscal")!!, call.argument<String>("horario")!!, call.argument<String>("operador")!!)
       "finishLastTransaction" -> tefMethods.finishLastTransaction(call.argument<Int>("confirma")!!)
